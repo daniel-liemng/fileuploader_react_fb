f9734c50942f4349561d511d34601a1cab0cbca6
Hoang Liem Nguyen <daniel.liemng@gmail.com>
2020-03-11 02:45:19 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from "firebase/app";
+import "firebase/storage";
+import FileUploader from "react-firebase-file-uploader";
+import config from "./firebase-config";
+
+firebase.initializeApp(config);
+
+class App extends Component {
+  // state
+  state = {
+    image: "",
+    imageURL: "",
+    progress: 0
+  };
+
+  // functions
+  handleUploadStart = () => {
+    this.setState({
+      progress: 0
+    });
+  };
+
+  handleUploadSuccess = filename => {
+    this.setState({
+      image: filename,
+      progress: 100
+    });
+
+    firebase
+      .storage()
+      .ref("avatars")
+      .child(filename)
+      .getDownloadURL()
+      .then(url =>
+        this.setState({
+          imageURL: url
+        })
+      );
+  };
+
+  handleProgress = progress => {
+    this.setState({ progress });
+  };
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div>
+        <label>Progress</label>
+        <p>{this.state.progress}</p>
+        <br />
+        <label>Image</label>
+        <br />
+        {this.state.image && (
+          <img
+            src={this.state.imageURL}
+            alt=""
+            style={{ height: "300px", width: "400px" }}
+          />
+        )}
+        <br />
+        {this.state.imageURL && <a href={this.state.imageURL}>Download Me</a>}
+        <br />
+        <FileUploader
+          accept="image/*"
+          name="image"
+          storageRef={firebase.storage().ref("avatars")}
+          onUploadStart={this.handleUploadStart}
+          onUploadSuccess={this.handleUploadSuccess}
+          onProgress={this.handleProgress}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyChXsUW8fe2zWhWgbCCP9srr-ARNxwNYKQ",
+  authDomain: "fileupload-react.firebaseapp.com",
+  databaseURL: "https://fileupload-react.firebaseio.com",
+  projectId: "fileupload-react",
+  storageBucket: "fileupload-react.appspot.com",
+  messagingSenderId: "519840407524",
+  appId: "1:519840407524:web:3bba7ae1b38914584171f1"
+};
+
+export default config;
